c46ffada8761f2f3f0a47b3f3641457bffa0b00d
yun0205yun <q0926250912@gmail.com>
2023-12-15 12:29:47 +0800

    需要全部排序

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,16 +13,18 @@ namespace Attractions.Controllers
         private const int PageSize = 5;
         private readonly Service _service;
 
-        // 使用依賴注入
+        // 這個建構函式使用了依賴注入的方式，接受一個 Service 類別的實例作為參數。
         public HomeController(Service service)
         {
+            //如果 service 參數為 null，則將拋出一個 ArgumentNullException(空引數例外處理)。
             _service = service ?? throw new ArgumentNullException(nameof(service));
         }
 
         // 空的無參數建構函式
         public HomeController()
         {
-            _service = new Service(); // 初始化 _service，這裡假設 Repository 也具有無參數建構函式
+            _service = new Service();
+            // 如果未提供 Service，則使用無參數建構函式初始化。這樣做的目的是為了確保即使未提供 Service，應用程式仍能運行，避免因缺少相依性而導致的執行時錯誤。
         }
 
         // 顯示全部景點的頁面
@@ -30,21 +32,24 @@ namespace Attractions.Controllers
         {
             try
             {
+                // 如果 page 為空，則使用 GetValueOrDefault() 方法將其設置為默認值。
                 int correctedPage = page.GetValueOrDefault();
-                ViewBag.searchText = searchText;
 
-                // 使用排序參數
+                // 將搜索文本存儲到 ViewBag 中，以在視圖中使用。
+                ViewBag.searchText = searchText;
+                ViewBag.sortBy = sortBy;
+                ViewBag.sortOrder = sortOrder;
+                // 使用排序參數調用 GetPagedMessagesResult 方法，獲取分頁的景點信息。
                 var paginatedMessages = GetPagedMessagesResult(searchText, correctedPage, PageSize, null, null, sortBy, sortOrder);
 
+                // 使用 ToPagedList 方法將分頁結果轉換為分頁對象，並將其存儲在 pages 變數中。
                 var pages = paginatedMessages.Messages.ToPagedList(correctedPage, PageSize, paginatedMessages.TotalMessages);
-
-                // 將排序參數存儲到 ViewBag 中，以便在 View 中使用
-                ViewBag.SortOrder = sortOrder;
-
+                // 返回 View，將分頁對象作為參數傳遞。
                 return View(pages);
             }
             catch (Exception ex)
             {
+                // 如果發生異常，調用 HandleError 方法處理異常，同時返回包含空數據的分頁對象。
                 HandleError(ex, "前端操作出錯");
                 return View(new PagedList<InformationDataModel>(new List<InformationDataModel>(), 1, PageSize));
             }
@@ -53,24 +58,32 @@ namespace Attractions.Controllers
         // 用來換部分頁面
         public ActionResult AjaxPage(string searchText, int? page, List<string> selectedAreas, List<string> selectedCities, string sortBy, string sortOrder)
         {
+            // 如果 page 為空，則將其設置為 1。
             int correctedPage = page ?? 1;
-            ViewBag.searchText = searchText;
 
+            // 將搜索文本存儲到 ViewBag 中，以在視圖中使用。
+            ViewBag.searchText = searchText;
+             
             try
             {
+                // 使用 GetPagedMessagesResult 方法獲取分頁的景點信息。
                 var paginatedMessages = GetPagedMessagesResult(searchText, correctedPage, PageSize, selectedAreas, selectedCities, sortBy, sortOrder);
 
+                // 如果是 AJAX 請求，返回部分視圖；否則，返回完整視圖。
                 if (Request.IsAjaxRequest())
                 {
+                    // 如果是 AJAX 請求，使用 PartialView 方法返回名為 "partialArea" 的部分視圖。
                     return PartialView("partialArea", paginatedMessages.Messages.ToPagedList(correctedPage, PageSize, paginatedMessages.TotalMessages + 1));
                 }
                 else
                 {
+                    // 如果不是 AJAX 請求，直接使用 View 方法返回完整視圖。
                     return View(paginatedMessages.Messages.ToPagedList(correctedPage, PageSize, paginatedMessages.TotalMessages + 1));
                 }
             }
             catch (Exception ex)
             {
+                // 如果發生異常，調用 HandleError 方法處理異常，同時返回包含空數據的部分視圖。
                 HandleError(ex, "AjaxPage 方法出錯");
                 // 記錄詳細的錯誤信息
                 ModelState.AddModelError("", "無法顯示留言。");
@@ -78,47 +91,64 @@ namespace Attractions.Controllers
             }
         }
 
-        // 來判斷是輸入文字搜尋  還是沒有輸入
+
+        // 來判斷是輸入文字搜尋還是沒有輸入
         private PagedMessagesResult<InformationDataModel> GetPagedMessagesResult(string searchText, int correctedPage, int pageSize, List<string> selectedAreas, List<string> selectedCities, string sortBy, string sortOrder)
         {
+            // 如果搜索文字為空或為 null，則執行未搜索的分頁查詢。
             if (string.IsNullOrEmpty(searchText))
             {
+                // 調用 _service 的 GetPagedMessages 方法，獲取分頁的景點信息。
                 return _service.GetPagedMessages(correctedPage, pageSize, sortBy, sortOrder);
             }
             else
             {
-                return _service.SearchAttractions(searchText, correctedPage, pageSize, selectedAreas, selectedCities);
+                // 如果有搜索文字，則執行帶有搜索條件的分頁查詢。
+                // 調用 _service 的 SearchAttractions 方法，使用搜索條件獲取分頁的景點信息。
+                return _service.SearchAttractions(searchText, correctedPage, pageSize, selectedAreas, selectedCities, sortBy, sortOrder);
             }
         }
+
 
         // 新增景點
         public ActionResult Create()
         {
+            // 創建一個 CreateModel 對象，用於在視圖中顯示相關的表單。
             CreateModel model = new CreateModel();
+
+            // 返回包含 CreateModel 對象的視圖，該視圖將用於展示新增景點的相關表單。
             return View(model);
         }
+
 
         // 創建新增景點
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(CreateModel createModel)
         {
+            // 檢查模型的驗證狀態
             if (!ModelState.IsValid)
             {
+                // 如果模型驗證失敗，返回包含錯誤信息的視圖
                 return View(createModel);
             }
 
+            // 檢查用戶是否已經登錄
             if (Session["Username"] == null)
             {
+                // 如果未登錄，重定向到登錄頁面
                 return RedirectToAction("Loggin", "Account");
             }
+
             try
             {
+                // 獲取當前登錄用戶的UserId
                 int userId = (int)Session["UserId"];
 
+                // 創建新的 CreateModel 對象，包含用戶提交的景點信息
                 var newMessage = new CreateModel
                 {
-                    CreateUserID = userId,
+                    CreateUserID = userId,//這裡新增人設為登入人ID
                     CreatedAt = DateTime.Now,
                     CategoryName = createModel.CategoryName,
                     CityName = createModel.CityName,
@@ -126,32 +156,45 @@ namespace Attractions.Controllers
                     AttractionDesc = createModel.AttractionDesc
                 };
 
+                // 調用 _service 的 AddMessage 方法，將新的景點信息添加到數據庫
                 _service.AddMessage(newMessage);
 
+                // 新增成功，重定向到顯示所有景點的頁面
                 return RedirectToAction("AttractionInformation", "Home");
             }
             catch (Exception ex)
             {
+                // 處理異常情況，記錄錯誤信息
                 HandleError(ex, $"Create 操作出錯: {ex.Message}");
+
+                // 在模型狀態中添加錯誤信息，並返回包含錯誤信息的視圖
                 ModelState.AddModelError("", "無法創建留言。");
                 return View(createModel);
             }
         }
 
+
         // 依照區域分類取得城市
         [HttpGet]
         public ActionResult GetCities(string area)
         {
+            // 調用 CreateModel 的 GetCitiesByArea 靜態方法，根據區域名稱獲取城市列表
             List<SelectListItem> cities = CreateModel.GetCitiesByArea(area);
+
+            // 將城市列表以 JSON 格式返回，用於異步請求//JsonRequestBehavior.AllowGet允許來自用戶端的 HTTP GET 要求。
             return Json(cities, JsonRequestBehavior.AllowGet);
         }
 
         // 景點內容(按標題下去會出現描述)
         public ActionResult AttractionContent(string title)
         {
+            // 調用 _service 的 GetAttractionByTitle 方法，根據標題獲取景點信息
             var attraction = _service.GetAttractionByTitle(title);
+
+            // 返回一個視圖，視圖中包含景點信息
             return View(attraction);
         }
+
 
         // 刪除景點
         [HttpPost]
@@ -160,8 +203,10 @@ namespace Attractions.Controllers
         {
             try
             {
+                // 調用 _service 的 DeleteAttraction 方法，嘗試刪除指定 AttractionID 的景點
                 bool success = _service.DeleteAttraction(AttractionID);
 
+                // 根據刪除是否成功返回相應的 JSON 結果
                 if (success)
                 {
                     return Json(new { Success = true, Message = "景點刪除成功。" });
@@ -173,48 +218,71 @@ namespace Attractions.Controllers
             }
             catch (Exception ex)
             {
+                // 處理異常情況，記錄錯誤信息
                 HandleError(ex, $"DeleteAttraction Error: {ex.Message}");
+
+                // 返回包含錯誤信息的 JSON 結果
                 return Json(new { Success = false, Message = "刪除景點失敗。錯誤: " + ex.Message });
             }
         }
 
-        // 編輯更新景色
+        // 用於獲取現有景點的信息以顯示在編輯表單中
         public ActionResult EditPage(int AttractionID)
         {
+            // 根據 AttractionID 獲取對應的景點信息
             var attraction = _service.GetAttractionID(AttractionID);
+
+            // 返回包含景點信息的視圖，用於顯示編輯表單
             return View(attraction);
         }
 
-        [HttpPost]
+        [HttpPost]//處理提交的表單
         [ValidateAntiForgeryToken]
         public ActionResult EditPage(InformationDataModel attraction)
         {
             try
             {
+                // 獲取當前用戶的ID
                 int userId = (int)Session["UserId"];
+
+                // 調用 _service 的 EditPage 方法，嘗試更新景點信息
                 bool success = _service.EditPage(attraction);
+
+                // 設置編輯者的ID
                 attraction.EditUserID = userId;
+
+                // 根據更新是否成功執行相應的操作
                 if (success)
                 {
+                    // 如果成功，重定向到顯示全部景點的頁面
                     return RedirectToAction("AttractionInformation", "Home");
                 }
                 else
                 {
+                    // 如果失敗，返回包含錯誤信息的 JSON 結果
                     return Json(new { Success = false, Message = "編輯景點失敗。" });
                 }
             }
             catch (Exception ex)
             {
+                // 處理異常情況，記錄錯誤信息
                 HandleError(ex, $"EditPage Error: {ex.Message}");
+
+                // 返回包含錯誤信息的 JSON 結果
                 return Json(new { Success = false, Message = "編輯景點失敗。錯誤: " + ex.Message });
             }
         }
 
+        // 處理錯誤的私有方法
         private void HandleError(Exception ex, string message)
         {
+            // 在控制台中輸出錯誤信息
             Console.WriteLine($"{message}: {ex.Message}");
+
+            // 將錯誤信息添加到模型狀態中，用於顯示給用戶
             ModelState.AddModelError("", "無法顯示留言。");
         }
+
     }
 }
 
--- Service.cs
+++ Service.cs
@@ -1,10 +1,11 @@
-﻿using Attractions.Models;
+﻿using Attractions.Models.Information;
+using Attractions.Models;
+using Attractions.Tools;
+using System.Collections.Generic;
 using System;
 using System.Linq;
-using Attractions.Tools;
-using Attractions.Models.Information;
-using System.Collections.Generic;
-
+using System.Data.SqlClient;
+using System.Globalization;
 
 namespace Attractions
 {
@@ -12,36 +13,48 @@ namespace Attractions
     {
         private readonly Repository _repository;
 
+        // Service 類別的建構函式
         public Service()
         {
+            // 初始化 Repository 物件，用於訪問資料庫
             _repository = new Repository();
         }
 
-
-
+        // 獲取分頁消息的方法
         public PagedMessagesResult<InformationDataModel> GetPagedMessages(int page, int pageSize, string sortBy, string sortOrder)
         {
             try
             {
+                // 調用 Repository 的 GetPagedMessages 方法，獲取分頁消息
                 var messages = _repository.GetPagedMessages(page, pageSize, sortBy, sortOrder);
+
+                // 根據排序條件進行排序
                 SortMessages(messages, sortBy, sortOrder);
+
+                // 返回分頁消息結果
                 return messages;
             }
             catch (Exception ex)
             {
-
+                // 如果發生異常，輸出錯誤信息至控制台，並返回一個空的 PagedMessagesResult
                 Console.WriteLine($"GetPagedMessages Error: {ex.Message}");
                 return new PagedMessagesResult<InformationDataModel>();
             }
         }
+
+        // 根據排序條件對消息進行排序的私有方法
         private void SortMessages(PagedMessagesResult<InformationDataModel> messages, string sortBy, string sortOrder)
         {
             switch (sortBy)
             {
                 case "CreatedAt":
+                    // 如果排序條件為 "CreatedAt"
+                    // 根據 CreatedAt 屬性升序或降序排序消息列表
                     messages.Messages = sortOrder == "asc" ? messages.Messages.OrderBy(m => m.CreatedAt).ToList() : messages.Messages.OrderByDescending(m => m.CreatedAt).ToList();
                     break;
                 case "EditAt":
+                    // 如果排序條件為 "EditAt"
+                    // 根據 EditAt 屬性升序或降序排序消息列表
                     messages.Messages = sortOrder == "asc" ? messages.Messages.OrderBy(a => a.EditAt).ToList() : messages.Messages.OrderByDescending(a => a.EditAt).ToList();
                     break;
             }
@@ -49,85 +62,108 @@ namespace Attractions
 
 
         // 新增的搜尋景點方法的實現
-        public PagedMessagesResult<InformationDataModel> SearchAttractions(string searchText, int page, int pageSize, List<string> selectedAreas, List<string> selectedCities)
+        public PagedMessagesResult<InformationDataModel> SearchAttractions(string searchText, int page, int pageSize, List<string> selectedAreas, List<string> selectedCities, string sortBy, string sortOrder)
         {
             try
             {
-                // 請在這裡實現搜尋邏輯，可以參考 Repository 中的相應方法
-                var result = _repository.SearchAttractions(searchText, page, pageSize, selectedAreas, selectedCities);
-
+                // 調用 Repository 的 SearchAttractions 方法，實現搜尋邏輯
+                var result = _repository.SearchAttractions(searchText, page, pageSize, selectedAreas, selectedCities, sortBy, sortOrder);
+                SortMessages(result, sortBy, sortOrder);
+                // 返回搜尋結果
                 return result;
             }
             catch (Exception ex)
             {
+                // 如果發生異常，輸出錯誤信息至控制台，然後重新拋出異常
                 Console.WriteLine($"SearchAttractions Error: {ex.Message}");
                 throw;
             }
         }
 
+        // 根據標題獲取景點的方法
         public InformationDataModel GetAttractionByTitle(string title)
         {
+            // 調用 Repository 的 GetAttractionByTitle 方法，根據標題獲取景點信息
             var result = _repository.GetAttractionByTitle(title);
+
+            // 返回景點信息
             return result;
         }
 
-        // 新增景點
+        // 新增景點的方法
         public void AddMessage(CreateModel message)
         {
+            // 調用 Repository 的 Create 方法，新增景點
             _repository.Create(message);
         }
 
-        // 得到景點編號
+        // 根據景點編號獲取景點信息的方法
         public InformationDataModel GetAttractionID(int AttractionID)
         {
             try
             {
+                // 調用 Repository 的 GetAttractionID 方法，獲取指定 AttractionID 的景點信息
                 var message = _repository.GetAttractionID(AttractionID);
 
+                // 檢查是否找到了相應的消息
                 if (message == null)
                 {
+                    // 如果未找到，輸出錯誤信息至控制台和錯誤日誌
                     Console.WriteLine($"ContentId 為 {AttractionID} 的消息未找到");
                     ErrorLog.LogError($"ContentId 為 {AttractionID} 的消息未找到");
+
+                    // 將 message 設置為一個新的 InformationDataModel 對象，避免返回 null
                     message = new InformationDataModel();
                 }
 
+                // 返回找到的或新建的消息對象
                 return message;
             }
             catch
             {
+                // 如果發生異常，返回 null
                 return null;
             }
         }
 
-        // 刪除
+        // 刪除景點的方法
         public bool DeleteAttraction(int AttractionID)
         {
             try
             {
+                // 調用 Repository 物件的 DeleteAttraction 方法進行刪除操作
                 _repository.DeleteAttraction(AttractionID);
+
+                // 如果成功執行到這一行，表示刪除操作成功，返回 true
                 return true;
             }
             catch (Exception ex)
             {
+                // 如果發生異常，捕獲並輸出錯誤訊息至控制台
                 Console.WriteLine($"DeleteAttraction Error: {ex.Message}");
+
+                // 返回 false 表示刪除操作失敗
                 return false;
             }
         }
 
-        // 編輯
+        // 編輯景點信息的方法
         public bool EditPage(InformationDataModel attraction)
         {
             try
             {
+                // 調用 Repository 的 EditPage 方法，將傳入的 InformationDataModel 對象用於編輯景點信息
                 _repository.EditPage(attraction);
+
+                // 如果編輯操作成功，返回 true
                 return true;
             }
             catch (Exception ex)
             {
+                // 如果發生異常，記錄錯誤信息至控制台，然後重新拋出異常
                 Console.WriteLine($"EditPage Error: {ex.Message}");
                 throw;
             }
         }
     }
 }
-
